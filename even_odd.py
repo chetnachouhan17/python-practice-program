dc147572cf2957118f5125496036d04628bd19eb
chetnachouhan17 <101549317+chetnachouhan17@users.noreply.github.com>
2022-05-11 17:17:28 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,12 @@
+a=float(input("Enter a number"))
+#a= int(a)
+
+c=a%2
+print(c)
+if(a==0):
+   print('no is neither even nor odd')
+
+elif(a%2==0):
+    print('no is even')
+else:
+   print('no is odd')
