aca4a0fcb985dec25ac59aa802a28bd9854b9294
chetnachouhan17 <101549317+chetnachouhan17@users.noreply.github.com>
2022-05-11 22:00:18 +0530

    Add files via upload

---
+++ operatoroverloading.py
@@ -0,0 +1,11 @@
+class A:
+    def __init__(self,a):
+        self.a = a
+    def __add__(self,o):
+        return self.a + o.a
+ob1 = A(1)
+ob2 = A(2)
+ob3 = A("Chetna")
+ob4 = A("Chouhan")
+print(ob1 + ob2)
+print(ob3 + ob4)
