50ca53429367a048812a74af86bb635c4af5b29c
chetnachouhan17 <101549317+chetnachouhan17@users.noreply.github.com>
2022-05-11 22:07:04 +0530

    Add files via upload

---
+++ string_slicing.py
@@ -0,0 +1,2 @@
+a = "bablu, dablu"
+print(a[1:5])
---
+++ swapping.py
@@ -0,0 +1,16 @@
+#python program to swap two variables
+
+a = int(input("ENter first value"))
+b = int(input("ENter second value"))
+
+print("the value of a before swapping:",a)
+print("the value of b before swapping:",b)
+
+c = a
+a = b
+b = c
+print('')
+print("the value of a after swapping:",a)
+print("the value of b after swapping:",b)
+
+
