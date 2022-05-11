5366c7b721abe60fac49c356ab6bee0a0b8dc4b9
chetnachouhan17 <101549317+chetnachouhan17@users.noreply.github.com>
2022-05-11 21:55:48 +0530

    Add files via upload

---
+++ greaterno.py
@@ -0,0 +1,12 @@
+a=int(input('enter first number'))
+b=int(input('enter  second number'))
+c=int(input('enter third number'))
+
+if(a>b and a>c):
+    print('a is greater than b and c')
+elif(b>a and b>c):
+    print('b is greater than a and c')
+else:
+   print ('c is greater than a and b')
+    
+    
